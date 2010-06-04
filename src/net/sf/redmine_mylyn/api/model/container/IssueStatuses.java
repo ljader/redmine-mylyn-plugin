37a610a6f8b286ff23e9075ee23701291c1fe151
Sven Krzyzak <sven@svenk.(none)>
2010-06-04 20:57:02 +0200

    chg: IssueStatus (defaultStatus)

--- src/net/sf/redmine_mylyn/api/model/container/IssueStatuses.java
+++ src/net/sf/redmine_mylyn/api/model/container/IssueStatuses.java
@@ -16,13 +16,30 @@ public class IssueStatuses extends AbstractSortedPropertyContainer<IssueStatus> 
 
 	protected List<IssueStatus> issueStatus;
 	
+	protected IssueStatus defaultStatus;
+	
 	@Override
 	@XmlElement(name="issueStatus")
 	protected List<IssueStatus> getModifiableList() {
 		if(issueStatus==null) {
-			issueStatus = new ArrayList<IssueStatus>();
+			issueStatus = new ArrayList<IssueStatus>() {
+
+				private static final long serialVersionUID = 1L;
+				
+				@Override
+				public boolean add(IssueStatus e) {
+					if(defaultStatus==null || e.isDefault()) {
+						defaultStatus = e;
+					}
+					return super.add(e);
+				}
+				
+			};
 		}
 		return issueStatus;
 	}
 
+	public IssueStatus getDefault() {
+		return defaultStatus;
+	}
 }
