78fbcb15d769e78e9cf9fd9e00504da284fafca2
Sven Krzyzak <sven@svenk.(none)>
2010-09-14 21:26:20 +0200

    add: log code

--- net.sf.redmine_mylyn.api/src/net/sf/redmine_mylyn/api/exception/RedmineApiErrorException.java
+++ net.sf.redmine_mylyn.api/src/net/sf/redmine_mylyn/api/exception/RedmineApiErrorException.java
@@ -1,25 +1,17 @@
 package net.sf.redmine_mylyn.api.exception;
 
+import java.text.MessageFormat;
+
 public class RedmineApiErrorException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
-	public RedmineApiErrorException(String message, String... params) {
-		super(placeParams(message, params));
+	public RedmineApiErrorException(String message, Object... params) {
+		super(MessageFormat.format(message, params));
 	}
 
-	public RedmineApiErrorException(String message, Throwable cause, String... params) {
-		super(placeParams(message, params), cause);
-	}
-	
-	private static String placeParams(String message, String[] params) {
-		for (String param : params) {
-			message.replaceFirst("{}", param==null ? "<NULL>" : param);
-		}
-		
-		message = message.replaceAll("\\{\\d+\\}", "");
-		
-		return message;
+	public RedmineApiErrorException(String message, Throwable cause, Object... params) {
+		super(MessageFormat.format(message, params), cause);
 	}
 	
 }
