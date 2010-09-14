12fe8146b3d6aa36d2ff619751b8c29d6beeb567
Sven Krzyzak <sven@svenk.(none)>
2010-09-14 20:48:07 +0200

    fix: old code removed add: log ioexception (connection refused)

--- net.sf.redmine_mylyn.core/src/net/sf/redmine_mylyn/core/RedmineCorePlugin.java
+++ net.sf.redmine_mylyn.core/src/net/sf/redmine_mylyn/core/RedmineCorePlugin.java
@@ -1,21 +1,18 @@
 package net.sf.redmine_mylyn.core;
 
+import java.text.MessageFormat;
+
 import net.sf.redmine_mylyn.common.logging.ILogService;
+import net.sf.redmine_mylyn.common.logging.LogServiceImpl;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.mylyn.commons.core.StatusHandler;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.log.LogEntry;
-import org.osgi.service.log.LogListener;
-import org.osgi.service.log.LogReaderService;
-import org.osgi.service.log.LogService;
 
-public class RedmineCorePlugin extends Plugin implements LogListener /*implements BundleActivator*/ {
+public class RedmineCorePlugin extends Plugin /*implements BundleActivator*/ {
 
 	private static BundleContext context;
 
@@ -26,12 +23,6 @@ public class RedmineCorePlugin extends Plugin implements LogListener /*implement
 	private static RedmineCorePlugin plugin;
 	
 	private RedmineRepositoryConnector connector;
-	
-	private ServiceReference logServiceReference;
-	private LogService logService;
-	
-	private ServiceReference logReaderServiceReference;
-	private LogReaderService logReaderService;
 	
 	static BundleContext getContext() {
 		return context;
@@ -47,17 +38,6 @@ public class RedmineCorePlugin extends Plugin implements LogListener /*implement
 		
 		RedmineCorePlugin.context = bundleContext;
 		plugin = this;
-		
-		logServiceReference = context.getServiceReference(LogService.class.getName());
-		if(logServiceReference!=null) {
-			logService = (LogService)context.getService(logServiceReference);
-		}
-
-		logReaderServiceReference = context.getServiceReference(LogReaderService.class.getName());
-		if(logReaderServiceReference!=null) {
-			logReaderService = (LogReaderService)context.getService(logReaderServiceReference);
-			logReaderService.addLogListener(this);
-		}
 	}
 
 	/*
@@ -66,15 +46,6 @@ public class RedmineCorePlugin extends Plugin implements LogListener /*implement
 	 */
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		if (logReaderServiceReference!=null) {
-			logReaderService.removeLogListener(this);
-			context.ungetService(logReaderServiceReference);
-		}
-
-		if (logServiceReference!=null) {
-			context.ungetService(logServiceReference);
-		}
-		
 		if(connector!=null) {
 			connector.getClientManager().writeCache();
 		}
@@ -83,20 +54,6 @@ public class RedmineCorePlugin extends Plugin implements LogListener /*implement
 		super.stop(bundleContext);
 	}
 
-	@Override
-	public void logged(LogEntry entry) {
-		int severity = entry.getLevel()==LogService.LOG_ERROR ? IStatus.ERROR : IStatus.INFO;
-		
-		IStatus status = new Status(severity, entry.getBundle().getSymbolicName(), entry.getMessage(), entry.getException());
-		if (entry.getLevel()==LogService.LOG_ERROR) {
-			StatusHandler.fail(status);
-		} else {
-			StatusHandler.log(status);
-		}
-		
-		//TODO logfile
-	}
-	
 	public static RedmineCorePlugin getDefault() {
 		return plugin;
 	}
@@ -111,21 +68,15 @@ public class RedmineCorePlugin extends Plugin implements LogListener /*implement
 	}
 
 	public ILogService getLogService(Class<?> clazz) {
-		return null;
+		return LogServiceImpl.getInstance(getBundle(), clazz);
 	}
 	
 	public static IStatus toStatus(Throwable e, String message) {
 		return new Status(IStatus.ERROR, PLUGIN_ID, message, e);
 	}
 
-	public static IStatus toStatus(Throwable e, String message, String... params) {
-		for (int i = 0; i < params.length; i++) {
-			message.replace("{"+i+"}", params[i]==null ? "<NULL>" : params[i]);
-		}
-		
-		//unused placeholders
-		message = message.replaceAll("\\{\\d+\\}", "");
-		
+	public static IStatus toStatus(Throwable e, String message, Object... params) {
+		message = MessageFormat.format(message, params);
 		return toStatus(e, message);
 	}
 
