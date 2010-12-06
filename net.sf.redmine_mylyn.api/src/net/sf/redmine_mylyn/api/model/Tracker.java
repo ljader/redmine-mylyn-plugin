215ee7ccbbba90ae56649a806fccd9f1094d5a8c
Sven Krzyzak <sven@svenk-hp.(none)>
2010-12-06 06:36:56 +0100

    add: Tracker contains now assigned IssueCUstomFields

--- net.sf.redmine_mylyn.api/src/net/sf/redmine_mylyn/api/model/Tracker.java
+++ net.sf.redmine_mylyn.api/src/net/sf/redmine_mylyn/api/model/Tracker.java
@@ -2,6 +2,8 @@ package net.sf.redmine_mylyn.api.model;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlList;
 import javax.xml.bind.annotation.XmlType;
 
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -9,5 +11,18 @@ import javax.xml.bind.annotation.XmlType;
 public class Tracker extends SortedProperty {
 
 	private static final long serialVersionUID = 1L;
+	
+	@XmlList
+	@XmlElement(name="issueCustomFields")
+	private int []issueCustomFields;
+
+	public int[] getIssueCustomFields() {
+		if(issueCustomFields==null) {
+			issueCustomFields = new int[0];
+		}
+		return issueCustomFields;
+	}
+	
+	
 
 }
